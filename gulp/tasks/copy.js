38f94c1cbe6166a947e59cd0ef268610804e6e1a
Sergey <seregacoder@gmail.com>
2020-11-11 15:56:01 +0400

    Сохранение пустых каталогов, добавление шрифтов eot, ttf

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   $.gulp.task('copy', function () {
     return $.gulp.src([
-        'src/fonts/**/*.{woff,woff2}',
+        'src/fonts/**/*.{woff,woff2,ttf,eot}',
         'src/img/**'
     ], {
       base: './src'
